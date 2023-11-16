879273343c4df7305548152ac213990587a49147
JasIrvinGoogle <jasmineirvin@google.com>
2023-11-16 15:45:09 -0500

    Main Solved problem 1

--- algorithmic_problem_solving_1.py
+++ algorithmic_problem_solving_1.py
@@ -1,3 +1,21 @@
 # Linear runtime solution
 # Iterate through each number in list = o(n)
 # Initialize/start at 0, go through each number in the list, update result -> for loop
+
+nums1 = [2,2,1]
+result1 = 0
+for num in nums1:
+    result1 ^= num #bitwise operation XOR - 'a = a^b'
+print(f"Example 1: {result1}")
+
+nums2 = [4,1,2,1,2]
+result2 = 0
+for num in nums2:
+    result2^= num
+print(f"Example 2: {result2}")
+
+nums3 = [1]
+result3 = 0
+for num in nums3:
+    result3^= num
+print(f"Example 3: {result3}")
